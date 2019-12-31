86effcdc0d18c7e03c07c480e73119a192f38a1c
Kasper Knapik <kasper.knapik@gmail.com>
2019-12-31 15:22:38 +0100

    Added config.json with config informations, such as tokens.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-import { default_token } from 'token';
-
 const Discord = require('discord.js');
-const token = default_token;
+const config = require('./config.json');
 const bot_client = new Discord.Client();
 
 bot_client.on('ready', () => {
@@ -9,8 +7,23 @@ bot_client.on('ready', () => {
 });
 
 bot_client.on('message', msg => {
-    if (msg.content === 'hello') {
-        msg.reply('hello world!');
+    if (
+        msg.author.bot === false &&
+        msg.channel.name === 'memes' &&
+        msg.content !== ''
+    ) {
+        msg.reply('This is not the channel for that!')
+            .then(reply => {
+                reply.delete(3000);
+            });
+        msg.delete(1000);
+    }
+
+    if (msg.content === '!delete' &&
+        msg.author.tag === 'TheToster#3003'
+    ) {
+        msg.channel.bulkDelete(10);
     }
 });
-bot_client.login(token);
+
+bot_client.login(config.token);
